0744d406e7486c83807f0323a83f1b283c1a50c5
Alex Smith <asmith@curse.com>
2013-10-08 17:33:21 -0500

    Round 5 of extension updates
    --HG--
    branch : mediawiki121

--- RegexFunctions.i18n.magic.php
+++ RegexFunctions.i18n.magic.php
@@ -19,6 +19,13 @@ $magicWords['ar'] = array(
 	'rreplace' => array( 0, 'استبدال_ريجيكس' ),
 );
 
+/** German (Deutsch) */
+$magicWords['de'] = array(
+	'rmatch' => array( 0, 'r_zuordnen' ),
+	'rsplit' => array( 0, 'r_aufteilen' ),
+	'rreplace' => array( 0, 'r_ersetzen' ),
+);
+
 /** Japanese (日本語) */
 $magicWords['ja'] = array(
 	'rmatch' => array( 0, '正規表現一致' ),
@@ -26,14 +33,35 @@ $magicWords['ja'] = array(
 	'rreplace' => array( 0, '正規表現置き換え' ),
 );
 
-/** Macedonian (Македонски) */
+/** Korean (한국어) */
+$magicWords['ko'] = array(
+	'rmatch' => array( 0, 'r찾기' ),
+	'rsplit' => array( 0, 'r분할' ),
+	'rreplace' => array( 0, 'r바꾸기' ),
+);
+
+/** Macedonian (македонски) */
 $magicWords['mk'] = array(
 	'rmatch' => array( 0, 'рсовпадни' ),
 	'rsplit' => array( 0, 'родвој' ),
 	'rreplace' => array( 0, 'рзамени' ),
 );
 
-/** Nedersaksisch (Nedersaksisch) */
+/** Malayalam (മലയാളം) */
+$magicWords['ml'] = array(
+	'rmatch' => array( 0, 'ചേരുമ്പടി' ),
+	'rsplit' => array( 0, 'വിഘടിപ്പിക്കൽ' ),
+	'rreplace' => array( 0, 'മാറ്റിച്ചേർക്കൽ' ),
+);
+
+/** Marathi (मराठी) */
+$magicWords['mr'] = array(
+	'rmatch' => array( 0, 'रिजुळवा', 'रजुळवा' ),
+	'rsplit' => array( 0, 'रितोडा', 'रतोडा' ),
+	'rreplace' => array( 0, 'रिबदला', 'रबदला' ),
+);
+
+/** Low Saxon (Netherlands) (Nedersaksies) */
 $magicWords['nds-nl'] = array(
 	'rmatch' => array( 0, 'rvergelieken' ),
 );
@@ -45,14 +73,14 @@ $magicWords['nl'] = array(
 	'rreplace' => array( 0, 'rvervangen' ),
 );
 
-/** Serbian (Cyrillic script) (‪Српски (ћирилица)‬) */
+/** Serbian (Cyrillic script) (српски (ћирилица)‎) */
 $magicWords['sr-ec'] = array(
 	'rmatch' => array( 0, 'рпоклопи' ),
 	'rsplit' => array( 0, 'рраздвоји', 'рподели' ),
 	'rreplace' => array( 0, 'рзамени' ),
 );
 
-/** Serbian (Latin script) (‪Srpski (latinica)‬) */
+/** Serbian (Latin script) (srpski (latinica)‎) */
 $magicWords['sr-el'] = array(
 	'rmatch' => array( 0, 'rpoklopi' ),
 	'rsplit' => array( 0, 'rrazdvoji', 'rpodeli' ),
