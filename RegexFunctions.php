81f83d719a5f633fc80c727974c619c1280b1c95
Alexia E. Smith <washuu@gmail.com>
2018-04-30 12:12:11 -0500

    Whoops.

--- RegexFunctions.php
+++ RegexFunctions.php
@@ -13,7 +13,6 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'RegexFunctions' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['RegexFunctions'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['RegexFunctionsAliases'] = __DIR__ . '/RegexFunctions.alias.php';
 	/* wfWarn(
 		'Deprecated PHP entry point used for RegexFunctions extension. ' .
 		'Please use wfLoadExtension instead, ' .
